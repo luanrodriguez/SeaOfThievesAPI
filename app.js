23d30ecea8cbcf91f043c881f319ba0743a88f34
Luan Rodrigues <luanrodriguesdev@gmail.com>
2022-08-22 23:52:18 -0300

    começando a lidar com documentação (swagger)

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 import express from "express";
 import { routes } from "./routes.js";
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
 
 const app = express();
 
+const swaggerOptions = {
+    swaggerDefinition:{
+        info: {
+            title: 'Sea of Thieves API',
+            description: 'This API provides information about Sea of Thieves video-game',
+            contact: {
+                name: 'Luan'
+            },
+            servers:["http://localhost:5600"]
+        }
+    },
+    apis:['routes.js']
+}
+
+const swaggerDocs = swaggerJSDoc(swaggerOptions);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 app.use(express.json());
 app.use(routes);
 
