64a3a82be6bd0bbae569a7508a392aa14bffa2fb
Luan <luanrodriguesdev@gmail.com>
2022-08-26 16:16:03 -0300

    senha deletada do retorno do cadastro, adicionado procfile

--- src/modules/login/LoginController.js
+++ src/modules/login/LoginController.js
@@ -1,37 +1,34 @@
-import { UsuariosRepository } from "../usuarios/UsuariosRepository.js"
-import { comparePassword } from "../../utils/PasswordHandler.js"
-import jwt from "jsonwebtoken"
-const usuariosRepository = new UsuariosRepository()
+import { UsuariosRepository } from "../usuarios/UsuariosRepository.js";
+import { comparePassword } from "../../utils/PasswordHandler.js";
+import jwt from "jsonwebtoken";
+const usuariosRepository = new UsuariosRepository();
 
 const internalErrorMessage = { fail: "Ocorreu um erro" };
 
-class LoginController{
-    async postLogin(req, res){
-        try{
-            const {usuario, senha} = req.body;
-            const user = await usuariosRepository.select(usuario)
-            if(!user.dataValues.usuario){
-                return res.status(404).json({fail: "Usuário não encontrado"})
-            }
+class LoginController {
+  async postLogin(req, res) {
+    try {
+      const { usuario, senha } = req.body;
+      const user = await usuariosRepository.select(usuario);
+      if (!user.dataValues.usuario) {
+        return res.status(404).json({ fail: "Usuário não encontrado" });
+      }
 
-            if(await comparePassword(senha, user.dataValues.senha)){
-                const token = jwt.sign(
-                    {
-                      usuario,
-                    },
-                    "secret",
-                    {expiresIn:30}
-                  );
-                return res.status(201).json({ token });
-            }
-            return res.status(403).json({fail: "Senha inválida"})
-            
-        } catch(err){
-            console.log(err)
-            return res.status(500).json(internalErrorMessage);
-        }
-        
+      if (await comparePassword(senha, user.dataValues.senha)) {
+        const token = jwt.sign(
+          {
+            usuario,
+          },
+          process.env.JWT_SECRET,
+          { expiresIn: 30 }
+        );
+        return res.status(201).json({ token });
+      }
+      return res.status(403).json({ fail: "Senha inválida" });
+    } catch (err) {
+      return res.status(500).json(internalErrorMessage);
     }
+  }
 }
 
-export {LoginController}
+export { LoginController };
--- src/modules/usuarios/UsuariosController.js
+++ src/modules/usuarios/UsuariosController.js
@@ -1,20 +1,21 @@
-import { UsuariosRepository } from "./UsuariosRepository.js"
-import { hashPassword, comparePassword } from "../../utils/PasswordHandler.js"
-const repository = new UsuariosRepository()
+import { UsuariosRepository } from "./UsuariosRepository.js";
+import { hashPassword } from "../../utils/PasswordHandler.js";
+const repository = new UsuariosRepository();
 
 const internalErrorMessage = { fail: "Ocorreu um erro" };
 
-class UsuariosController{
-    async postUsuarios(req, res){
-        try{
-            const {usuario, senha} = req.body;
-            const senhaHash = await hashPassword(senha)
-            return res.status(201).json(await repository.create({usuario, senha: senhaHash}))
-        } catch {
-            return res.status(500).json(internalErrorMessage);
-        }
-        
+class UsuariosController {
+  async postUsuarios(req, res) {
+    try {
+      const { usuario, senha } = req.body;
+      const senhaHash = await hashPassword(senha);
+      const response = await repository.create({ usuario, senha: senhaHash });
+      delete response.dataValues.senha;
+      return res.status(201).json(response);
+    } catch {
+      return res.status(500).json(internalErrorMessage);
     }
+  }
 }
 
-export {UsuariosController}
+export { UsuariosController };
